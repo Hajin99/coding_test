dc1543329b169400e6cf74a305c3d319ecf2ff8d
김하진 <114723246+Hajin99@users.noreply.github.com>
2024-03-29 18:37:29 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 120 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -1,20 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int A = Integer.parseInt(st.nextToken()); // Climbing distance per day
-        int B = Integer.parseInt(st.nextToken()); // Sliding distance per night
-        int V = Integer.parseInt(st.nextToken()); // Total distance to climb
-
-        // Calculate the number of days
-        int day = (V - B - 1) / (A - B) + 1;
-
-        System.out.println(day);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine()) ;
+    	
+	    int A = Integer.parseInt(st.nextToken()); 
+	    int B = Integer.parseInt(st.nextToken());
+	    int V = Integer.parseInt(st.nextToken());
+		int day=0;
+		
+		//day = (V - B -1) / (A  - B)+1;
+		day = (V  - B) / (A - B);
+		if((V  - B) % (A - B)!=0)
+			day++;
+		System.out.println(day);
+	}
 }

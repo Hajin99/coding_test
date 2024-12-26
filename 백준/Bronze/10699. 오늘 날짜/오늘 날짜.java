1c5471e63f1ef6155613c9f6b3a1554599cbbe92
김하진 <114723246+Hajin99@users.noreply.github.com>
2024-12-26 12:42:25 +0900

    [Bronze V] Title: 오늘 날짜, Time: 152 ms, Memory: 17240 KB -BaekjoonHub

--- 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -1,26 +1,11 @@
-import java.util.Calendar;
+import java.time.LocalDate;
 
-public class Main {
-	public static void main(String[] args) {
-		Calendar now = Calendar.getInstance();
-		int year = now.get(Calendar.YEAR);
-		int month = now.get(Calendar.MONTH)+1;
-		int day = now.get(Calendar.DAY_OF_MONTH);
-		
-		if(month>10) {
-			if(day>10)
-				System.out.println(year + "-" + month + "-" + day );
-			else {
-				System.out.println(year + "-" + month + "-0" + day);
-			}
-		}
-		else {
-			if(day>10) {
-				System.out.println(year + "-0" + month + "-" + day);
-			}
-			else {
-				System.out.println(year + "-0" + month + "-0" + day);
-			}
-		}
-	}
+public class Main{
+    public static void main(String [] args){
+        LocalDate today = LocalDate.now();
+        System.out.println(today);
+        //LocalDateTime now = LocalDateTime.now();
+        //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-mm-dd");
+        //System.out.println(today.format(formatter));
+    }
 }
